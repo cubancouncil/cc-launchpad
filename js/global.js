dd81684b28e257de8cea36836a5817f3ac3bdc60
Chris Forrette <chris@chrisforrette.com>
2012-04-12 18:36:41 -0700

    Adding launchpad site skeleton

---
+++ js/global.js
@@ -0,0 +1,26 @@
+/**
+ * ProjectName
+ * Handles all global behaviors and properties for the site
+ *
+ * Dependencies:
+ * - jQuery 1.72
+ *
+ */
+
+(function($) {
+
+    var ProjectName = {
+
+        init: function() {
+
+        }
+
+    };
+
+    window.ProjectName = ProjectName;
+
+    $(document).ready(function() {
+        ProjectName.init();
+    });
+
+})(jQuery);
